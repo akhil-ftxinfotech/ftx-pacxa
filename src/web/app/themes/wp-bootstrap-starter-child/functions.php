80e2cf530403ac1607488d220f5c8f8c5abccd59
akhil-ftxinfotech <akhil.mokariya@ftxinfotech.com>
2021-03-08 15:39:27 +0530

    Latest Change

--- src/web/app/themes/wp-bootstrap-starter-child/functions.php
+++ src/web/app/themes/wp-bootstrap-starter-child/functions.php
@@ -1,5 +1,4 @@
-<?php
- 
+<?php 
 	 add_action( 'wp_enqueue_scripts', 'wp_bootstrap_starter_child_enqueue_styles' );
 	 function wp_bootstrap_starter_child_enqueue_styles() {
  		  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
