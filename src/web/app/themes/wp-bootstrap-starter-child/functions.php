d4b564be80755322422804e83fc2938cf5d6e932
akhil-ftxinfotech <akhil.mokariya@ftxinfotech.com>
2021-03-08 19:47:39 +0530

    commit the changes

--- src/web/app/themes/wp-bootstrap-starter-child/functions.php
+++ src/web/app/themes/wp-bootstrap-starter-child/functions.php
@@ -1,6 +1,8 @@
 <?php 
-	 add_action( 'wp_enqueue_scripts', 'wp_bootstrap_starter_child_enqueue_styles' );
-	 function wp_bootstrap_starter_child_enqueue_styles() {
- 		  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
- 		  } 
+	add_action( 'wp_enqueue_scripts', 'wp_bootstrap_starter_child_enqueue_styles' );
+	function wp_bootstrap_starter_child_enqueue_styles() {
+ 		wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+		wp_register_style('style', get_stylesheet_directory_uri() . '/dist/app.css');
+ 		wp_enqueue_style('style'); 
+ 	} 
  ?>
